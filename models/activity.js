6f8e08ca88c6b3f8f853be0afa7ea5257a8bdcc8
paulcmd <choxmail.pm@gmail.com>
2020-06-30 21:01:20 -0700

    activity model

---
+++ models/activity.js
@@ -0,0 +1,63 @@
+module.exports = function (sequelize, DataTypes) {
+    var Activity = sequelize.define("Activity", {
+        date: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+        },
+        notes: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                len: [1]
+            }
+        },
+        care: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                len: [1]
+            }
+        },
+        light: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                len: [1]
+            }
+        },
+        status: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                len: [1]
+            }
+        },
+        water: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                len: [1]
+            }
+        },
+        fertilizer: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                len: [1]
+            }
+        }
+
+    });
+    Activity.associate = function (models) {
+        // Associating Activity with a Plant
+        // When an Plant is deleted, also delete any associated Activity
+        Activity.belongsTo(models.Plant, {
+            onDelete: "cascade"
+        })
+    };
+
+
+
+    return Activity;
+};
+
