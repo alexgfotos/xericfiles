dbaee42ee0db7f57b84f95c3d1927d1ae429ac11
alex g <alexgfotos@gmail.com>
2020-06-30 18:26:10 -0700

    assocations added

--- models/genus.js
+++ models/genus.js
@@ -1,32 +1,33 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     var Genus = sequelize.define("Genus", {
-      genus: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
+        genus: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        country: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+            len: [1]
         }
-      },
-      country: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-        len: [1]
-      }
     });
-  
-    Genus.associate = function(models) {
+
+    Genus.associate = function (models) {
         // Associating Genus with Posts
         // When an Genus is deleted, also delete any associated Posts
         Genus.hasMany(models.Species, {
-          onDelete: "cascade"
-        }),
+            onDelete: "cascade"
+        })
         Genus.hasMany(models.Image, {
             onDelete: "cascade"
-          }),
+        })
         Genus.hasOne(models.Zone
-          );
-      };
-  
+        );
+    };
+
+
+
     return Genus;
-  };
-  
+};
--- models/plant.js
+++ models/plant.js
@@ -1,45 +1,52 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     var Plant = sequelize.define("Plant", {
-      date: {
-        type: DataTypes.DATEONLY,
-        allowNull: false,
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        validate: {
-          len: [1]
-        }
-      },
-      width: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        len: [1]
-      },
-      price: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        len: [1]
-      },
-      notes: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-        validate: {
-          len: [1]
-        }
-      },
+        date: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                len: [1]
+            }
+        },
+        width: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            len: [1]
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            len: [1]
+        },
+        notes: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+            validate: {
+                len: [1]
+            }
+        },
     });
-  
-    Plant.associate = function(models) {
-        // Associating Plant with Posts
-        // When an Plant is deleted, also delete any associated Posts
+
+    Plant.associate = function (models) {
         Plant.hasMany(models.Image, {
             onDelete: "cascade"
-          }),
-        Plant.belongsTo(models.User
-          );
-      };
-  
-    return Plant;
-  };
-  
+        })
+        Plant.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+        Plant.belongsTo(models.Species, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+
+    }
+
+    return Plant
+
+};
