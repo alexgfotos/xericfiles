4944b653349b52d402828c5ffcd8d05b39f95e46
FCTESLA <fcamacho888@gmail.com>
2020-07-02 22:09:53 -0700

    auto complete

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
 module.exports={
   "development": {
     "username": "root",
-    "password": "burger12?",
+    "password": "Daniela8!",
     "database": "xeric_files",
     "host": "127.0.0.1",
     "dialect": "mysql"
