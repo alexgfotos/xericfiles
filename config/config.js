0f23f9f99158e6c9aa61200c2254660ab4b0a649
alex g <alexgfotos@gmail.com>
2020-07-11 11:52:27 -0700

    config fix

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports={
+    "development": {
+      "username": "root",
+      "password": process.env.PASSWORD,
+      "database": "xeric_files",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    },
+    "test": {
+      "username": "root",
+      "password": process.env.PASSWORD,
+      "database": "xeric_files_test",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    },
+    "production":{
+      "username": process.env.USERNAME,
+      "password": process.env.PASSWORD,
+      "database": process.env.DATABASE,
+      "host": process.env.HOST,
+      "dialect": "mysql"
+    }
+  }
