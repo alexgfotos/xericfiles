46cf4800cadd747197406e363a0faf00dc8cba3d
Cassy Pearson <seepearson@gmail.com>
2020-07-11 11:26:58 -0700

    background and homepage

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
 module.exports={
   "development": {
     "username": "root",
-    "password": "burger12?",
+    "password": "rootroot",
     "database": "xeric_files",
     "host": "127.0.0.1",
     "dialect": "mysql"
