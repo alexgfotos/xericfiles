eb0e844b611b97d3fa052d443e8fa20a1c6660e5
FCTESLA <fcamacho888@gmail.com>
2020-07-18 09:31:07 -0700

    getting genus.genus

--- client/src/components/PlantCard.js
+++ client/src/components/PlantCard.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
-import {Grid, ListItemText, Card, CardHeader, CardMedia, CardContent, CardActions, Collapse, Avatar, IconButton, Typography } from '@material-ui/core';
+import { Grid, ListItemText, Card, CardHeader, CardMedia, CardContent, CardActions, Collapse, Avatar, IconButton, Typography } from '@material-ui/core';
 import { red } from '@material-ui/core/colors';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
@@ -68,7 +68,7 @@ export default function PlantCard() {
 
     async function getGenus() {
 
-      const gen = await axios.get(`/api/species?GenusId=${species.GenusId}`)
+      const gen = await axios.get(`/api/genus/${species.GenusId}`)
       console.log(gen);
 
       setGenusOptions(gen.data);
@@ -127,102 +127,109 @@ export default function PlantCard() {
 
 
   return (
+    <>
+     
+        <Card className={classes.root} >
 
-    <Card className={classes.root} >
-      <CardHeader
-        avatar={
-          <Avatar alt="plant" src={image} className={classes.small} />
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title={"Species: " + species.species}
-        subheader={plant.name}
-      />
-      <CardMedia
-        className={classes.media}
-        image={image}
-        title="plant image"
-      />
-      <CardContent>
-        <Grid container spacing={2} >
-          <Grid item xs={6} md={6} container direction="column" justify="flex-start" alignItems="flex-start"  >
-            <ListItemText
-              primary={"Nickname: " + plant.name}
-            />
-            <ListItemText
-              primary={"Date: " + plant.date}
-            />
-            <ListItemText
-              primary={"Width: " + plant.width + " inches"}
-            />
-            <ListItemText
-              primary={"Price: $" + plant.price}
-            />
+          <CardHeader
+            avatar={
+              <Avatar alt="plant" src={image} className={classes.small} />
+            }
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title={genus.genus}
+            subheader={plant.name}
+          />
+          <CardMedia
+            className={classes.media}
+            image={image}
+            title="plant image"
+          />
+          <CardContent>
+            <Grid container spacing={2} >
+              <Grid item xs={6} md={6} container direction="column" justify="flex-start" alignItems="flex-start"  >
 
-          </Grid>
+                <ListItemText
+                  primary={"Nickname: " + plant.name}
+                />
+                <ListItemText
+                  primary={"Date: " + plant.date}
+                />
+                <ListItemText
+                  primary={"Width: " + plant.width + " inches"}
+                />
+                <ListItemText
+                  primary={"Price: $" + plant.price}
+                />
 
-          <Grid item xs={6} md={6} container direction="column" justify="center" alignItems="flex-start" >
-            <ListItemText
-              primary={"Genus: " + "g"}
-            />
-            <ListItemText
-              primary={"Species: " + species.species}
-            />
-            <ListItemText
-              primary="Single-line item"
-            />
-            <ListItemText
-              primary="Single-line item"
-            />
-          </Grid>
-        </Grid>
-        <Typography variant="body2" color="textSecondary">
-          This cactus consists of many small tubercles growing from a large tap root. They are usually solitary, rarely giving rise to side shoots from old areoles.
+              </Grid>
+
+              <Grid item xs={6} md={6} container direction="column" justify="center" alignItems="flex-start" >
+                <ListItemText
+                  primary={"Genus: " + genus}
+                />
+                <ListItemText
+                  primary={"Species: " + species.species}
+                />
+                <ListItemText
+                  primary="Single-line item"
+                />
+                <ListItemText
+                  primary="Single-line item"
+                />
+              </Grid>
+
+            </Grid>
+
+            <Typography variant="body2" color="textSecondary">
+              This cactus consists of many small tubercles growing from a large tap root. They are usually solitary, rarely giving rise to side shoots from old areoles.
         </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton
+              className={clsx(classes.expand, {
+                [classes.expandOpen]: expanded,
+              })}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography paragraph>Method:</Typography>
+              <Typography paragraph>
+                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+                minutes.
           </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+              <Typography paragraph>
+                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+                heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+                browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+                and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
+                pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+                saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
           </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
+              <Typography paragraph>
+                Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+                without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+                medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+                again without stirring, until mussels have opened and rice is just tender, 5 to 7
+                minutes more. (Discard any mussels that don’t open.)
           </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
+              <Typography>
+                Set aside off of the heat to let rest for 10 minutes, and then serve.
           </Typography>
-        </CardContent>
-      </Collapse>
+            </CardContent>
+          </Collapse>
 
-    </Card>
+        </Card>
+    
+    </>
   );
 }
